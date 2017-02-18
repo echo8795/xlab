3c2ef9f783b73320118165198ad364c88272f1e4
vidur <vidur@navgurukul.org>
2017-02-18 15:42:19 +0530

    added demo authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthComponent } from './auth/auth.component';
+
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: AuthComponent},
+  // { path: '',
+  //   redirectTo: '/login',
+  //   pathMatch: 'full'
+  // }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
